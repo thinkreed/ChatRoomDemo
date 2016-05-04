165dc5854851a70fbccb19f7e4811834dcbbb2ac
thinkreed <thinkreed2016@gmail.com>
2016-05-04 10:29:37 +0800

    a amend

---
+++ app/src/main/java/com/trails/chatroomdemo/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.trails.chatroomdemo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.trails.chatroomdemo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
