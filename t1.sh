49003a813c905fbe4bc00eb1ae91457407436493
thinkreed <thinkreed2016@gmail.com>
2016-05-19 16:18:33 +0800

    rm t1.sh

--- t1.sh
+++
@@ -1,6 +0,0 @@
-git filter-branch --env-filter '
-  if [ $GIT_AUTHOR_EMAIL = weijie.hu@qianmo-inc.com ];
-          then GIT_AUTHOR_EMAIL=thinkreed2016@gmail.com;
-                fi;
-                export GIT_AUTHOR_EMAIL'
-'
